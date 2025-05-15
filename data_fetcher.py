0838ab509e43c1d814f71f4f6170c6b2d2228303
Vladtepesch <vladtepesch@Mac.fritz.box>
2025-05-15 16:32:55 +0200

    created a .env file and implemented dotenv funktion

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,14 @@
 import json
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv("secret.env")
 
 def fetch_data(user_animal_input):
-    API_KEY = 'UN4D5vRBaN8ybUMEfq5x3g==OQ4jGHiWAkmE0Zxf'
+    API_KEY = os.getenv("API_KEY")
     SEARCH = user_animal_input
     url = f'https://api.api-ninjas.com/v1/animals?name={SEARCH}'
     res = requests.get(url, headers={'X-Api-Key': API_KEY})
     return json.loads(res.text)
+
