8feb1a9d3a83db6e3026862c2eaa1d3baa0ee779
Vladtepesch <vladtepesch@Mac.fritz.box>
2025-05-15 11:38:31 +0200

    Data from API not JSON File implemented

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,10 +1,14 @@
 import json
+import requests
 
 
-def load_data(file_path):
-    """ Loads a JSON file """
-    with open(file_path, encoding="UTF-8", errors="ignore") as handle:
-        return json.load(handle)
+def load_data():
+    """ Gets Data from API """
+    API_KEY = 'UN4D5vRBaN8ybUMEfq5x3g==OQ4jGHiWAkmE0Zxf'
+    SEARCH = "Fox"
+    url = f'https://api.api-ninjas.com/v1/animals?name={SEARCH}'
+    res = requests.get(url, headers={'X-Api-Key': API_KEY})
+    return json.loads(res.text)
 
 
 def load_template(file_path):
@@ -13,23 +17,23 @@ def load_template(file_path):
         return gettemp.read()
 
 
-def serialize_animal(fox):
+def serialize_animal(animal):
     """Serialization of the animal objects"""
     output = ""
     output += "<li class='cards__item'>\n"
-    output += f"<div class='card__title'>{fox["name"]}<br/>\n</div><br/>"
+    output += f"<div class='card__title'>{animal["name"]}<br/>\n</div><br/>"
     try:
-        output += f"<div class='card__text'>\n <ul>\n<li><strong>Diet: </strong>{fox["characteristics"]["diet"]}</li>\n"
+        output += f"<div class='card__text'>\n <ul>\n<li><strong>Diet: </strong>{animal["characteristics"]["diet"]}</li>\n"
     except KeyError:
         pass
     try:
-        output += f"<li><strong>Prey: </strong>{fox["characteristics"]["prey"]}</li>"
+        output += f"<li><strong>Prey: </strong>{animal["characteristics"]["prey"]}</li>"
     except KeyError:
         pass
     output += "<li><strong>Location: </strong>"
-    output += f" {", ".join(fox["locations"])}</li>\n"
+    output += f" {", ".join(animal["locations"])}</li>\n"
     try:
-        output += f"<li><strong>Type: </strong>{fox["characteristics"]["type"]}</li>\n"
+        output += f"<li><strong>Type: </strong>{animal["characteristics"]["type"]}</li>\n"
     except KeyError:
         pass
     output += "</ul>"
@@ -39,11 +43,11 @@ def serialize_animal(fox):
 
 
 def combine_data_template():
-    animals_data = load_data("animals_data.json")
+    animals_data = load_data()
     template = load_template("animals_template.html")
     output = ""
-    for fox in animals_data:
-        output += serialize_animal(fox)
+    for animal in animals_data:
+        output += serialize_animal(animal)
     temp_with_data = template.replace("__REPLACE_ANIMALS_INFO__", output)
     return temp_with_data
 
